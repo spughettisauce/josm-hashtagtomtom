3a2afac17df58fe06fb78a9612ddbdd99f5724dc
spughetti <163912344+spughettisauce@users.noreply.github.com>
2024-10-16 16:14:15 +0200

    Update pTerryUploadHook.java
    Changed changeset tag

--- src/org/technomancy/openstreetmap/pterry/pTerryUploadHook.java
+++ src/org/technomancy/openstreetmap/pterry/pTerryUploadHook.java
@@ -8,6 +8,6 @@ import org.openstreetmap.josm.actions.upload.UploadHook;
 public class pTerryUploadHook implements UploadHook {
     @Override
     public void modifyChangesetTags(Map<String, String> tags) {
-        tags.put("clacks_overhead", "GNU Terry Pratchett");
+        tags.put("hashtags", "#tomtom");
     }
 }
